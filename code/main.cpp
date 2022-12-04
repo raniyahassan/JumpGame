44e31c09b55725623b9c3c996d2d69128ca384d6
admin <admin@BCS-Ubuntu-34.lrccd.ad.losrios.edu>
2022-12-04 02:43:25 -0800

    commit

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main()
 {
+    double width = sf::VideoMode::getDesktopMode().width;
+    double height = sf::VideoMode::getDesktopMode().height; 
+
+    sf::Event event;
+    sf::VideoMode VideoWindow(width, height);
+    sf::RenderWindow window(VideoWindow, "JumpGame", Style::Default); 
+
+
     return 0;
 }
