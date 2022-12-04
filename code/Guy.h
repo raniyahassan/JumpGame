4eb59a419644c5ea69deeaa37d28fd2f64956819
admin <admin@BCS-Ubuntu-34.lrccd.ad.losrios.edu>
2022-12-04 04:41:59 -0800

    no

---
+++ code/Guy.h
@@ -0,0 +1,18 @@
+#ifndef GUY
+#define GUY
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+class Guy 
+{
+    private:
+        int x;
+    public:
+        Guy(); 
+};
+
+#endif GUY
--- code/main.cpp
+++ code/main.cpp
@@ -13,6 +13,12 @@ int main()
     Texture background; 
     background.loadFromFile("images/background.png"); 
     Sprite back(background); 
+
+    Texture playerTexture;
+    playerTexture.loadFromFile("images/tempGuy.png");
+    Sprite player(playerTexture); 
+    player.setPosition(Vector2f{172 , 500}); 
+
     Screen s; 
     RectangleShape backs = s.square(); 
 
@@ -20,18 +26,34 @@ int main()
     VideoMode VideoWindow(686, 966);
     RenderWindow window(VideoWindow, "JumpGame" ); 
 
+    enum State {START, PLAYING, END}; 
+    State current = START; 
+
     while (window.isOpen())
     {
         while (window.pollEvent(event))
         {
+
+            window.draw(back); 
+
             if (Keyboard::isKeyPressed(Keyboard::Escape)) {window.close();}
 
             if (event.type == Event::Closed) {window.close();}
+
+            if (current == START)
+            {
+                window.clear();
+                window.draw(backs);
+
+                // window.draw(text); 
+                // if (start button pressed) {current == PLAYING}
+                
+            }
+
         }
 
         window.clear();
-        window.draw(back); 
-        window.draw(backs); 
+        window.draw(player); 
         window.display(); 
     }
 
